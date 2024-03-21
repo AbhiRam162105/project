c1ad7b0c178b94465744bc253b205730d908c3a5
Aditya <adityakrsingh2604@gmail.com>
2024-03-21 18:47:06 +0530

    profile section user details add

--- git-clone/src/components/Profile/Profile.jsx
+++ git-clone/src/components/Profile/Profile.jsx
@@ -4,9 +4,28 @@ import "./Profile.css"
 
 
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+    return (
+        <div className='profile-page-wrapper'>
+
+            <div className="user-profile-section">
+                <div className="profile-image">
+                </div>
+
+                <div className="name">
+                    <h3>Name</h3>
+                    <p>username</p>
+                </div>
+
+                <button className='follow-btn'>Follow</button>
+
+                <div className="follower">
+                    <p>10 Follower</p>
+                    <p>3 Following</p>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Profile
